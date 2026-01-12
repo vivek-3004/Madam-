8fd4ee3a4ee419f29a53ab1ce96ebd82c0e7e588
vivek-3004 <forworkonly3004@gmail.com>
2026-01-13 00:49:18 +0530

    Add BackgroundMusic component for audio playback

---
+++ BackgroundMusic.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+
+interface Props {
+  play: boolean;
+}
+
+const BackgroundMusic: React.FC<Props> = ({ play }) => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    if (!audioRef.current) {
+      audioRef.current = new Audio(require(""));
+      audioRef.current.loop = true;
+      audioRef.current.volume = 0.5;
+    }
+
+    if (play) {
+      audioRef.current.play().catch(() => {});
+    } else {
+      audioRef.current.pause();
+    }
+  }, [play]);
+
+  return null;
+};
+
+export default BackgroundMusic;
